c3ffb6c1d66e7b34c2180742e99445f6e877f9e5
Grae Abbott <grae@umich.edu>
2018-06-13 11:24:42 -0500

    flickrDownload completed

--- flickrDownload.py
+++ flickrDownload.py
@@ -7,37 +7,55 @@
 
 from flickrapi import FlickrAPI  # https://pypi.python.org/pypi/flickrapi
 import urllib
-import os
+import os, sys,time
 #import config
 from random import randint
-import time
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+#google drive auth process
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
 
 API_KEY = "6ab5883201c84be19c9ceb0a4f5ba959"  #// replace with your key
 API_SECRET = "1d2bcde87f98ed92"  #// replace with your secret
-IMG_FOLDER = "/Users/affanfarid/Desktop/flickrdownloadimages/"
+IMG_FOLDER = "/mnt/c/Users/ghabb/Desktop/GitHub/WildbookFlickrStats/" 
+
+folder_id = '1eJeXfRx8VxDRLT8ttdEgjC4eWUa258M1' #folder id for "Shared with me/social media bias/data/Grevy's"
+resultsID = '1Wdxca3CdSw9TvSIvPJhr-wLM2FxdmgKt' #folder id for ^^/"Grevy's Search Results"
 
 def download_flickr_photo(url, keyword, count):
     results_folder = IMG_FOLDER #+ keyword.replace(" ", "_") + "/"
     if not os.path.exists(results_folder):
         os.makedirs(results_folder)
+
+    filename = str(count) + ".jpg"
     
     try:
         #downloads image
-        urllib.request.urlretrieve(url,  results_folder + str(count) +".jpg")
-        print('Downloading image #' + str(count) + ' from url ' + url)
+        urllib.urlretrieve(url,  results_folder + filename)
+        print 'Downloading image #' + str(count) + ' from url ' + url
 
-        #ADD UPLOAD LINES HERE
+        #UPLOAD LINES HERE
 
+        file = drive.CreateFile({'title' : filename, 'parents' : [{'kind' : 'drive#fileLink', 'id' : resultsID}]})
+        file.SetContentFile(filename)
+        file.Upload()
+
+        
         #deletion line
-        #os.remove( IMG_FOLDER + count + ".jpg")
+        os.remove( IMG_FOLDER + filename)
 
     except Exception as e:
-        print(e, 'Download failure')
+        print str(e) + ' Download failure'
 
 
 
 
-
+'''
 def download_flickr_photos(keywords, size='original', max_nb_img=-1):
     if not (isinstance(keywords, str) or isinstance(keywords, list)):
         raise AttributeError('keywords must be a string or a list of strings')
@@ -95,13 +113,13 @@ def download_flickr_photos(keywords, size='original', max_nb_img=-1):
                 print('Downloading image #' + str(count) + ' from url ' + url)
             except Exception as e:
                 print(e, 'Download failure')
-        print("Total images downloaded:", str(count - 1))
-
+        print ("Total images downloaded:", str(count - 1))
+'''
 #testing function
 
-'''
+
 #download_flickr_photos(keywords="grevy's zebra",max_nb_img = 5)
 
 url1 = "https://farm1.staticflickr.com/893/40731835074_ccfbbefb59_o.jpg"
 download_flickr_photo(url1, "test", 100)
-'''
+
